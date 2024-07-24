22d07b7acc520cbe43c8e856e91e9e36714ae9d6
Maxim Rozanov <m.rozanoov@gmail.com>
2024-07-24 11:16:07 +0300

    corrections_5

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q, Count
+from django.db.models import Q,
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
